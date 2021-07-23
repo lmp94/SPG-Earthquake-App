1bdc013e5aa3cc2d782ba7220afe9e2ea3615935
Larissa Perara <larissaperara@gmail.com>
2021-07-23 09:25:16 -0700

    Initial Commit

---
+++ SPG-Take-Home-Earthquake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPG-Take-Home-Earthquake
+//
+//  Created by Larissa Perara on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
